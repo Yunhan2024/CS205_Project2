4ec70f66b8331697e59e3614246158b7078bc17c
Yunhan2024 <ychan268@ucr.edu>
2025-06-04 19:54:38 -0700

    neareast neighbor

--- NN.py
+++ NN.py
@@ -1,8 +1,12 @@
 import numpy as np
+import time
 
 def load_data(filename):
     data = np.loadtxt(filename)
     return data[:, 1:], data[:, 0]
 
-def forward_selection(features, classes):
+def nearest_neighbor(train_data, test_instance, features):
+    distances = np.sqrt(np.sum((train_data[:, features] - test_instance[features])**2, axis=1))
+    return np.argmin(distances)
+
 
